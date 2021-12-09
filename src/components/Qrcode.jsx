2653cacf1b3c55c7a1a90cad876ce1eabe5e8edc
Fernando Segregio <segregio@gmail.com>
2021-12-09 16:17:34 -0300

    Acerta texto e css qrcode + botão

--- src/components/Qrcode.jsx
+++ src/components/Qrcode.jsx
@@ -10,7 +10,7 @@ class Qrcode extends React.Component {
            <img className="foto-code1" src={ qrcode} alt="" />
            <h3>Para fazer sua contribuição:</h3>
            <h3>Leia o QR Code, ou clique no botão abaixo</h3> 
-           <h3>para abrie em seu App Financeiro</h3> 
+           <h3>para abrir em seu App Financeiro</h3> 
            <a href="https://nubank.com.br/pagar/swdn1/r8FHsXH45J"  target="_blank" rel="noreferrer"><button className="button-code"type="button">IR PARA O MEU APP</button></a>
         </div>
     );
