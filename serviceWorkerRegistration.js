1814622a16ea1308be1d6a9bb2ca072c4cd0b298
Fernando Segregio <segregio@gmail.com>
2022-01-14 19:52:48 -0300

    acerta app

--- serviceWorkerRegistration.js
+++ serviceWorkerRegistration.js
@@ -11,9 +11,9 @@
 // opt-in, read https://cra.link/PWA
 
 const isLocalhost = Boolean(
-  window.location.hostname === 'localhost' ||"https://natalgalacticos2021.vercel.app",
+  window.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.
-  window.location.hostname === '[::1]' ||"https://natalgalacticos2021.vercel.app",
+  window.location.hostname === '[::1]' ||
     // 127.0.0.0/8 are considered localhost for IPv4.
     window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
 );
