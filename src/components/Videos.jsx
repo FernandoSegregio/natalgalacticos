38612a9d9a2f3d88de4847cfab812a8abe3de3ab
FernandoSegregio <segregio@gmail.com>
2022-11-29 13:04:21 -0300

    autoplay in video

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -4,7 +4,7 @@ class Videos extends React.Component {
   render() {
     return (
       <div className="embed-container">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/hfw3FQ8reDE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/hfw3FQ8reDE?autoplay=1" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
       </div>
     );
   }
